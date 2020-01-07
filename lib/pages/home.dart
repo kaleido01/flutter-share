71c05fe1bf1cdfc37828c2124c25806d72ed00e3
kaleido01 <mbdkaleido@gmail.com>
2020-01-08 07:30:12 +0900

    create home design

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  Widget buildAuthScreen() {
+    return Text("Authenticated");
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).accentColor.withOpacity(.2),
+              Theme.of(context).primaryColor,
+            ])),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Flutter share",
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 90,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              child: Container(
+                width: 260,
+                height: 60,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage("assets/images/google_signin_button.png"),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
