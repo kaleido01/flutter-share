943afd2930f50e0b568453d4a824584a69808f23
kaleido01 <mbdkaleido@gmail.com>
2020-01-22 09:22:28 +0900

    add heder

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershareapp/widgets/header.dart';
+import 'package:fluttershareapp/widgets/progress.dart';
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,6 +10,7 @@ class Timeline extends StatefulWidget {
 class _TimelineState extends State<Timeline> {
   @override
   Widget build(context) {
-    return Text("Timeline");
+    return Scaffold(
+        appBar: header(context, isAppTitle: true), body: linearProgress());
   }
 }
